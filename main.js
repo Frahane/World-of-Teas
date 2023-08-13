8b65054801c40caca12ceac9d663a8a28a84d838
Frahane <Frahanebio@gmail.com>
2023-08-13 17:31:15 +0100

    Web dev

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.menu-toggle').click(function() {
+    $(this).toggleClass('active');
+  });
+  
+  
+  $('.scalable-image').click(function() {
+    $(this).toggleClass('scaled');
+  });
+  
+    $('.scalable').click(function() {
+    $(this).toggleClass('scaling');
+  });
+
+});
